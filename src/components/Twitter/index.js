1af5b76fdcaa2979aa0d860987f00189e9d54645
leidyolinto <suverleide.olinto@gmail.com>
2023-02-08 21:08:49 -0300

    implementando CSS twitter responsividade  a HOME

--- src/components/Twitter/index.js
+++ src/components/Twitter/index.js
@@ -3,35 +3,41 @@ import React from 'react';
 import CardsTwitter from "../CardsTwitter";
 
 
-function Twitter(props){
+function Twitter(props) {
   return (
     <div className="containerTwuiter">
+      {/* <button class="btnTwitter">
+        Twitter
+      </button> */}
       <div className="containerCard">
-        <CardsTwitter cardsTwitter={props.cardsTwitter}/>
+        <CardsTwitter cardsTwitter={props.cardsTwitter} />
+        {/* <button class="btnCard">
+          Imagens
+        </button> */}
       </div>
       <div className="containerInformation">
-        
+
         {
-          props.userTwitter.map((item,index) => <div className="twitterUser" key={index}>
-          <div className="twitterProfile">
-            <img src= {item.profile} alt= "foto do perfil" className="twitterPicture"/>
-          </div>
-          <div className="twitterInformations">
-            <div className="informationsUser"><strong>{item.user}:</strong> @{item.userName}</div>
-            <div className="informationsText">{item.text}</div>
-            <div className="informationsBoxLink">
-              <a href={item.twitter}           
-                 className="informationsLink" 
-                 target="_blank" 
-                 rel="noopener noreferrer">
+          props.userTwitter.map((item, index) => <div className="twitterUser" key={index}>
+            <div className="twitterProfile">
+              <img src={item.profile} alt="foto do perfil" className="twitterPicture" />
+            </div>
+            <div className="twitterInformations">
+              <div className="informationsUser"><strong>{item.user}:</strong> @{item.userName}</div>
+              <div className="informationsText">{item.text}</div>
+              <div className="informationsBoxLink">
+                <a href={item.twitter}
+                  className="informationsLink"
+                  target="_blank"
+                  rel="noopener noreferrer">
                   <strong>
                     Link ver mais no Twitter
                   </strong>
-              </a>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        )}
+          )}
       </div>
     </div>
   );
